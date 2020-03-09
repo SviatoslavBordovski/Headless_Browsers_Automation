a29dd5e26c8b6843a2d3bd078605f12c9856685b
Sviatoslav Bordovski <sbordovski@gmail.com>
2020-03-09 22:51:34 +0200

    Update headless_chrome.py

--- headless_chrome.py
+++ headless_chrome.py
@@ -7,19 +7,17 @@ chrome_options.add_argument("--headless")
 
 #Assign the "--headless" value to the "chrome_options" that were declared at the beginning, give a path of chromedriver
 driver = webdriver.Chrome(chrome_options=chrome_options, executable_path="/home/sb/chromedriverlinux/chromedriver")
+driver.implicitly_wait(5) #Wait for the element detection on the page
 
 #Open the url
 driver.get('https://google.com')
 print(driver.title, 'website is launched') #result of running so we could see it in the terminal
-time.sleep(5) #Give it a while to download the page elements fully
 
 driver.find_element_by_name('q').send_keys('I love Python')
 print('Keys are sent')
-time.sleep(5)
 
 driver.find_element_by_name('btnK').click()
 print('Button clicked')
-time.sleep(5)
 
 driver.close()
 driver.quit()
