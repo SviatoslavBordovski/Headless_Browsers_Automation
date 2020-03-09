6ca60b117c5fca557680e015480677c83302c0c3
Sviatoslav Bordovski <sbordovski@gmail.com>
2020-03-09 22:52:11 +0200

    Update headless_chrome.py

--- headless_chrome.py
+++ headless_chrome.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
-import time
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.options import Options
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
