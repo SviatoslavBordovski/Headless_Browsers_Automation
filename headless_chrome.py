386b08fa6102a4a5e58334526bcbdc2481057d17
Sviatoslav Bordovski <sbordovski@gmail.com>
2020-05-20 22:31:43 +0300

    Update headless_chrome.py

--- headless_chrome.py
+++ headless_chrome.py
@@ -8,14 +8,14 @@ chrome_options.add_argument("--headless")
 
 #Assign the "--headless" value to the "chrome_options" that were declared at the beginning, give a path of chromedriver:
 driver = webdriver.Chrome(chrome_options=chrome_options, executable_path="/home/sb/chromedriverlinux/chromedriver")
-driver.implicitly_wait(5) #Wait for the element detection on the page
+driver.implicitly_wait(5) #wait for the element detection on the page
 
 #Open the target url:
 driver.get('https://google.com')
 print(driver.title, 'website is launched') #result of running so we could see it in the terminal
 
 #Find the field and type the text to find:
-driver.find_element_by_name('q').send_keys('I love Python')
+driver.find_element_by_name('q').send_keys('Python is a programming language')
 print('Keys are sent')
 
 #Click to start the search:
